ff7d0ef9bf95cdf3c568756aa4b7ba1c576fd046
hi0ki <ansarimehdi518@gmail.com>
2025-06-17 18:02:56 +0100

    push 1337

--- main.cpp
+++ main.cpp
@@ -1,35 +1,12 @@
 #include <iostream>
 #include <vector>
-class test : public std::exception
-{
-	test()
-	{
-		std::cout << "teeest\n";
-	}
-	
-	const char *what() const throw()
-	{
-		return "safdsfaf";
-	}
-};
-
+#include <stack>
+#include <deque>
 
 int main()
 {
-	std::vector<int> v;
+	std::stack<int> var;
 
-	v.push_back(7);
-	v.push_back(1);
-	v.push_back(20);
-	v.push_back(10);
-	v.push_back(15);
-	v.push_back(17);
-
-	std::sort(v.begin() + 1, v.end() - 1);
-
-	std::cout << "Sorted vector: ";
-	for (size_t i = 0; i < v.size(); i++)
-	{
-		std::cout << v[i] << " ";
-	}
+	
+	std::cout << var
 }
